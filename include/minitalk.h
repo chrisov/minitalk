598130400eb524e5774613ca42b2fd6d27cb54c7
dchrysov <j.chrisov@gmail.com>
2024-12-13 18:41:56 +0100

    Initial commit

---
+++ include/minitalk.h
@@ -0,0 +1,16 @@
+
+#ifndef SERVER_H
+# define SERVER_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <signal.h>
+
+# include <string.h>
+#include <stdlib.h>
+
+# define LENGTH 100
+
+char	msg[LENGTH];
+
+#endif
